7ab81e579eb5304a8a15c9678342c29a42cf7d79
Onur <onur_oz_dogan@hotmail.com>
2023-04-10 02:22:59 +0300

    Bir şeyleri düzeltitm. Model controller bağlandı

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Categories;
 
 class CategoriesController extends Controller
 {
     public function index()
     {
         return view('categories');
+    }
+
+    public function addUser()
+    {
+        $cat = new Categories;
     }
 }

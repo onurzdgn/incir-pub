b8ba182968fb5c2fad735fa2daa7a5c92be1ad1e
Onur <onur_oz_dogan@hotmail.com>
2023-04-09 16:38:57 +0300

    Created add new product (front-end)

--- resources/views/components/breadcrump.blade.php
+++ resources/views/components/breadcrump.blade.php
@@ -1,7 +1,7 @@
 <div>
     <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
 
-    <div class="container ms-0">
+    <div class="container ms-0 mb-4">
         <div class="row">
             <div class="col">
                 <h1 style="color: #e6d8cc;">{{ $title }}</h1>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -2,5 +2,62 @@
 @section('content')
 
 <x-breadcrump title="Ürünler" />
-       
+
+<div class="container ms-1">
+    <div class="row">
+        <div class="col">
+            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Yeni ürün ekle</button>
+        </div>
+    </div>
+</div>
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Yeni Ürün Ekle</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="mb-3">
+                        <label for="recipient-name" class="col-form-label">Ürün Adı:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Ürün Bilgisi:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Ürün Fotoğrafı:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Ürün Kategorisi:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Ürün Fiyatı:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Kapat</button>
+                <button type="button" class="btn btn-primary">Kayıt Et</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    var myModal = document.getElementById('myModal')
+    var myInput = document.getElementById('myInput')
+
+    myModal.addEventListener('shown.bs.modal', function() {
+        myInput.focus()
+    })
+</script>
+
 @endsection

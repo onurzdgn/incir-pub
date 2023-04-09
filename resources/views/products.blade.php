5fcca204b742a1e5105980e28511f9e953f6cce2
Onur <onur_oz_dogan@hotmail.com>
2023-04-09 16:51:40 +0300

    Add new user created (front-end)

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -51,13 +51,4 @@
     </div>
 </div>
 
-<script>
-    var myModal = document.getElementById('myModal')
-    var myInput = document.getElementById('myInput')
-
-    myModal.addEventListener('shown.bs.modal', function() {
-        myInput.focus()
-    })
-</script>
-
 @endsection
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -2,5 +2,57 @@
 @section('content')
 
 <x-breadcrump title="Kullanıcılar" />
+
+<div class="container ms-1">
+    <div class="row">
+        <div class="col">
+            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Yeni Kullanıcı Ekle</button>
+        </div>
+    </div>
+</div>
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Yeni Kullancı Ekle</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="mb-3">
+                        <label for="recipient-name" class="col-form-label">Kullanıcı Giriş Adı:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Kullanıcı Adı:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Kullanıcı Soyadı:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Kullanıcı Telefonu:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Kullanıcı Rütbe:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Kullanıcı Şifresi:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Kapat</button>
+                <button type="button" class="btn btn-primary">Ürün EKle</button>
+            </div>
+        </div>
+    </div>
+</div>
        
 @endsection

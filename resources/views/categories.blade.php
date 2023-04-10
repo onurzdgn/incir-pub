d9c922fff79f64cbc9dfbaf831337f28722873de
Onur <onur_oz_dogan@hotmail.com>
2023-04-10 12:22:46 +0300

    categoriler sayfasında still verildi

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -14,7 +14,7 @@
 <div class="container mt-5 ms-1">
     <div class="row">
         <div class="col">
-            <table id="example" class="table table-striped table-bordered" style="width:100%" >
+            <table id="example" class="table table-striped table-bordered-black" style="width:100%; background-color:#e6d8cc;">
                 <thead>
                     <tr>
                         <th>Kategori Adı</th>

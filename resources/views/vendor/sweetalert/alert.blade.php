6937e61aa95b78e301da274d8c1e3d47c636190b
Onur <onur_oz_dogan@hotmail.com>
2023-04-11 12:58:44 +0300

    SweetAlert2 error is fixed :D

--- resources/views/vendor/sweetalert/alert.blade.php
+++ resources/views/vendor/sweetalert/alert.blade.php
@@ -8,7 +8,7 @@
     @if (config('sweetalert.alwaysLoadJS') === false && config('sweetalert.neverLoadJS') === false)
         <script src="{{ $cdn ?? asset('vendor/sweetalert/sweetalert.all.js')  }}"></script>
     @endif
-    <script>
+    <script type="">
         Swal.fire({!! Session::pull('alert.config') !!});
     </script>
 @endif

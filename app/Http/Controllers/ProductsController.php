31dc4eaf0563c8dc2f6279594bdef62f26c44063
Onur <onur_oz_dogan@hotmail.com>
2023-04-11 23:19:58 +0300

    Düzenleme

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -26,32 +26,7 @@ class ProductsController extends Controller
 
     public function addProduct(Request $request)
     {
-        // $productName = $request->input('productName');
-        // $productInfo = $request->input('productInfo');
-        // $productImage = $request->file('productPic');
-        // $productCategory = $request->input('productCategory');
-        // $productIngredients = $request->input('productIngredients');
-        // $productPrice = (int) $request->input('productPrice');
-
-        // $imageName = $request->file('productPic')->getClientOriginalName();
-
-        // // $request->file('productPic')->pathAs('public/publicImages/products', $imageName);
-
-        
-
-        // $product = new Products;
-        // $product->name = $productName;
-        // $product->info = $productInfo;
-        // $product->image = $imageName;
-        // $product->category_id = $productCategory;
-        // $product->ingredients = $productIngredients;
-        // $product->price = $productPrice;
-        // $product->is_active = 1;
-        // if($request->hasFile('productPic')) {
-        //     $product->image = $request->productPic
-        //         ->storeAs('public/publicImages', $request->productPic->getClientOriginalName());
-        // }
-        // $product->save();
+       
 
         $imageName = $request->file('productPic')->getClientOriginalName();
 

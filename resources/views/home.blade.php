6c651fbf90906dd897732e3fa92b16610fdc0a95
Onur <onur_oz_dogan@hotmail.com>
2023-06-05 23:47:43 +0300

    home page will editing for demo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,56 @@
 
 <x-breadcrump title="Anasayfa" />
 
-       
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                    <h5 class="card-title">En Çok Ziyaret Edilen Sayfalar</h5>
+                </div>
+                <div class="card-body">
+                    <ol>
+                        <li>Yerli Kokteyller</li>
+                        <li>Ulusal Kokteyller</li>
+                        <li>Yiyecekler</li>
+                        <li>Şişe Biralar</li>
+                        <li>Shots</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                    <h5 class="card-title">En Çok Zaman Geçirilen Sayfalar</h5>
+                </div>
+                <div class="card-body">
+                    <ol>
+                        <li>Yiyecekler</li>
+                        <li>Yerli Kokteyller</li>
+                        <li>Şişe Biralar</li>
+                        <li>Ulusal Kokteyller</li>
+                        <li>Shots</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                    <h5 class="card-title">En Çok Ziyeret Eden Ülkeler</h5>
+                </div>
+                <div class="card-body">
+                    <ol>
+                        <li>Türkiye</li>
+                        <li>Rusya</li>
+                        <li>Ukranya</li>
+                        <li>İngiltere</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection

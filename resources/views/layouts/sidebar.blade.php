a3ed0e2cc68715dcc82d2d298545f5fcc85c43bf
Onur <onur_oz_dogan@hotmail.com>
2023-06-13 22:02:50 +0300

    New pages added not opening (now)

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,27 +5,34 @@
             <span class="nav_logo-name">BBBootstrap</span>
         </a> -->
         <div class="nav_list">
-            <a href="/" class="nav_link {{request()->is('/') ? 'active' : ''}}">
+            <a href="/" class="nav_link {{ request()->is('/') ? 'active' : '' }}">
                 <i class='fs-4 bi-house nav_icon'></i>
                 <span class="nav_name">Anasayfa</span>
             </a>
-            <a href="/categories" class="nav_link {{request()->is('categories') ? 'active' : ''}}">
+            <a href="/categories" class="nav_link {{ request()->is('categories') ? 'active' : '' }}">
                 <i class="fs-4 bi bi-diagram-2 nav_icon"></i>
                 <span class="nav_name">Kategoriler</span>
             </a>
-            <a href="/products" class="nav_link {{request()->is('products') ? 'active' : ''}}">
+            <a href="/products" class="nav_link {{ request()->is('products') ? 'active' : '' }}">
                 <i class="fs-4 bi bi-cup-straw nav_icon"></i>
                 <span class="nav_name">Ürünler</span>
             </a>
-            <a href="/users" class="nav_link {{request()->is('users') ? 'active' : ''}}">
+            <a href="" class="nav_link">
+                <i class="bi bi-arrow-down-up nav_icon"></i>
+                <span class="nav_name">Kategori Sıralama</span>
+            </a>
+            <a href="" class="nav_link">
+                <i class="bi bi-arrow-left-right"></i>
+                <span class="nav_name">Ürün Sıralama</span>
+            </a>
+            <a href="/users" class="nav_link {{ request()->is('users') ? 'active' : '' }}">
                 <i class="fs-4 bi bi-person-lines-fill nav_icon"></i>
                 <span class="nav_name">Kullanıcılar</span>
             </a>
-
         </div>
     </div>
     <div class="">
-        <a href="/profile" class="nav_link {{request()->is('profile') ? 'active' : ''}}">
+        <a href="/profile" class="nav_link {{ request()->is('profile') ? 'active' : '' }}">
             <i class="fs-4 bi bi-person-fill nav_icon"></i>
             <span class="nav_name">Profil</span>
         </a>
